733d47efd0537027658a977d9384325c3b068919
Charlie Plonski <chaz.plonski@gmail.com>
2024-08-25 19:45:32 -0400

    finished stanford lecture 2

--- VoicePad/ContentView.swift
+++ VoicePad/ContentView.swift
@@ -6,30 +6,41 @@ struct ContentView: View {
 //    @State private var audioRecorder: AVAudioRecorder?
     //@state allows the proeprty to be changed and it prompts the view to rerender whenever it changes
     @State var cardCount: Int = 4
-    let emojis : [String] = ["⚡️", "Ó","⚡️", "Ó","⚡️", "Ó"]
+    let emojis : [String] = ["⚡️", "Ó","⚡️", "Ó","⚡️", "Ó","⚡️", "Ó","⚡️", "Ó"]
     var body: some View {
         VStack{
-            HStack {
-                ForEach(0..<cardCount, id: \.self){ index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView{
+                cards
             }
-            .padding().foregroundColor(.orange)
+            Spacer()
+            cardCountAdjuster
             
-            HStack{
-                Button(action: {
-                    cardCount = max(cardCount - 1,1)
-                }, label: {
-                    Image(systemName: "folder.badge.minus")
-                })
-                Spacer()
-                Button(action: {
-                    cardCount = min(cardCount + 1 , emojis.count)
-                }, label: {
-                    Text("Add Card")
-                })
-            }.padding().font(.largeTitle)
         }
+        
+    }
+    var cardCountAdjuster : some View {
+            HStack{
+            Button(action: {
+                cardCount = max(cardCount - 1,1)
+            }, label: {
+                Image(systemName: "folder.badge.minus")
+            })
+            Spacer()
+            Button(action: {
+                cardCount = min(cardCount + 1 , emojis.count)
+            }, label: {
+                Text("Add Card")
+            })
+        }.padding().font(.largeTitle)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 123))]) {
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
+            }
+        }.padding().foregroundColor(.orange)
+        
     }
 }
 
@@ -39,20 +50,19 @@ struct CardView: View {
     var body: some View {
         ZStack(){
             let base : some View = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-            if(isFaceUp){
+            Group{
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 10)
                 Text(content).font(.largeTitle)
-            }
-            else{
-                base
-
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             isFaceUp.toggle()
         }
     }
 }
+
+
 #Preview {
     ContentView()
 }
--- VoicePad/VoicePad.swift
+++ VoicePad/VoicePad.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct VoicePadApp: App {
+struct VoicePad: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
