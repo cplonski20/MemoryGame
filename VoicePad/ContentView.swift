af5a5ec72078140d0b477d3fec26f9a1c25931e7
Charlie Plonski <chaz.plonski@gmail.com>
2024-08-25 01:24:42 -0400

    Initial Commit

---
+++ VoicePad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoicePad
+//
+//  Created by Charles Plonski on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("it begins")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoicePad/VoicePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicePadApp.swift
+//  VoicePad
+//
+//  Created by Charles Plonski on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
