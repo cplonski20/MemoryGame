09e88d0f7c57e315372b89d6299a944d5ad352e5
Charlie Plonski <chaz.plonski@gmail.com>
2024-08-25 18:19:21 -0400

    Adding and removing buttons

--- VoicePad/ContentView.swift
+++ VoicePad/ContentView.swift
@@ -2,39 +2,54 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    @State private var isRecording = false
-    @State private var audioRecorder: AVAudioRecorder?
+//    @State private var isRecording = false
+//    @State private var audioRecorder: AVAudioRecorder?
     //@state allows the proeprty to be changed and it prompts the view to rerender whenever it changes
-
+    @State var cardCount: Int = 4
+    let emojis : [String] = ["⚡️", "Ó","⚡️", "Ó","⚡️", "Ó"]
     var body: some View {
-        HStack {
-            CardView(isFaceUp: false)
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            HStack {
+                ForEach(0..<cardCount, id: \.self){ index in
+                    CardView(content: emojis[index])
+                }
+            }
+            .padding().foregroundColor(.orange)
             
+            HStack{
+                Button(action: {
+                    cardCount = max(cardCount - 1,1)
+                }, label: {
+                    Image(systemName: "folder.badge.minus")
+                })
+                Spacer()
+                Button(action: {
+                    cardCount = min(cardCount + 1 , emojis.count)
+                }, label: {
+                    Text("Add Card")
+                })
+            }.padding().font(.largeTitle)
         }
-        .padding().foregroundColor(.orange).imageScale(.large)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = true
-    
+    @State var isFaceUp: Bool = true
+    var content : String = "⚡️"
     var body: some View {
         ZStack(){
+            let base : some View = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
             if(isFaceUp){
-                //Creates a rounded rectangle that is white. We need this as we can't rely on background being white since back drop can change to dark in night mode
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                //Creates a rounded rectange that just has a strokeborder and it defautls to the foreground color
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 10)
-                Text("⚡️").font(.largeTitle)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 10)
+                Text(content).font(.largeTitle)
             }
             else{
-                RoundedRectangle(cornerRadius: 12)
+                base
 
             }
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
